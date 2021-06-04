a15eeaab6cf062b1a47ba8fc1ee88246b363664e
huge <826992656@qq.com>
2021-06-04 21:59:22 +0800

    personas-tags init

---
+++ jvm/bigdata/spark/personas-tags/tags-etl/src/test/scala/com/pingpukj/tags/test/EtlAppTest.scala
@@ -0,0 +1,17 @@
+package com.pingpukj.tags.test
+
+/**
+ * Author: HuZunJie
+ * Date: 2021-06-04 20:52
+ * Email: 826992656@qq.com
+ * Version: 0.0.1
+ * Desc:本模块初始化后，环境测试
+ */
+
+
+object EtlAppTest {
+  def main(args: Array[String]): Unit = {
+    println("hello,world......")
+  }
+
+}
